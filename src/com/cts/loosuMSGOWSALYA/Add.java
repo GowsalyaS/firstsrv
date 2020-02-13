c01165a6ab8a14c86971c98b189131e7f8a48e24
839858 <839858@PC182789.cts.com>
2020-02-13 12:54:52 +0530

    commited

---
+++ src/com/cts/loosuMSGOWSALYA/Add.java
@@ -0,0 +1,51 @@
+package com.cts.loosuMSGOWSALYA;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class addition
+ */
+@WebServlet("/addition")
+public class Add extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Add() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String n1=request.getParameter("num1");
+		String n2=request.getParameter("num2");
+		
+		int result=Integer.parseInt(n1.trim())+Integer.parseInt(n2.trim());
+		RequestDispatcher rd=request.getRequestDispatcher("/Result");
+		request.setAttribute("ResultData", result);
+		rd.forward(request, response);
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+}
